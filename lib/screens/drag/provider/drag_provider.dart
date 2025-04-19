a17bcb6c2b885f10eda7c369b79dc14fd489a6fe
Hedeve01 <nazari.fph@gmail.com>
2025-04-19 19:55:30 +0330

    feat: video player and drag and drop

---
+++ lib/screens/drag/provider/drag_provider.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DragProvider extends ChangeNotifier {
+  final List<Offset> _positions = [];
+  final double itemSpacing = 60;
+
+  int? _draggingIndex;
+  Offset? _draggingPosition;
+
+  List<Offset> get positions => _positions;
+  int? get draggingIndex => _draggingIndex;
+  Offset? get draggingPosition => _draggingPosition;
+
+  void createItem() {
+    final newOffset = Offset(150, _positions.length * itemSpacing + 100);
+    _positions.add(newOffset);
+    notifyListeners();
+  }
+
+  void startDragging(int index) {
+    _draggingIndex = index;
+    _draggingPosition = _positions[index];
+    notifyListeners();
+  }
+
+  void updateItemPosition(int index, Offset delta) {
+    if (index < 0 || index >= _positions.length) return;
+    _positions[index] += delta;
+    _draggingPosition = _positions[index];
+    notifyListeners();
+  }
+
+  void stopDragging() {
+    _draggingIndex = null;
+    _draggingPosition = null;
+    notifyListeners();
+  }
+
+  void deleteItem(int index) {
+    _positions.removeAt(index);
+    _draggingIndex = null;
+    _draggingPosition = null;
+    notifyListeners();
+  }
+}
+
+final storyProvider = ChangeNotifierProvider((ref) => DragProvider());
---
+++ lib/screens/drag/widget/drag_area.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:insta_video_player/screens/drag/provider/drag_provider.dart';
+
+class StatefulDragArea extends ConsumerWidget {
+  const StatefulDragArea({super.key});
+
+  bool isNearTrash(Offset? itemPosition, Offset trashCenter,
+      {double threshold = 130}) {
+    if (itemPosition == null) return false;
+    return (itemPosition - trashCenter).distance < threshold;
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final story = ref.watch(storyProvider);
+    final notifier = ref.read(storyProvider.notifier);
+    final size = MediaQuery.of(context).size;
+
+    final trashCenter = Offset(size.width / 2, size.height - 100);
+    final isNear = isNearTrash(story.draggingPosition, trashCenter);
+
+    return Stack(
+      children: [
+        Positioned.fill(child: Container(color: Colors.grey[100])),
+        for (int index = 0; index < story.positions.length; index++)
+          Positioned(
+            left: story.positions[index].dx,
+            top: story.positions[index].dy,
+            child: GestureDetector(
+              onPanStart: (_) => notifier.startDragging(index),
+              onPanUpdate: (details) {
+                notifier.updateItemPosition(index, details.delta);
+              },
+              onPanEnd: (_) {
+                if (isNearTrash(story.positions[index], trashCenter)) {
+                  notifier.deleteItem(index);
+                } else {
+                  notifier.stopDragging();
+                }
+              },
+              child: Column(
+                children: [
+                  Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.green,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ),
+          ),
+        Positioned(
+          bottom: 40,
+          left: size.width / 2 - 65,
+          child: isNear
+              ? Image.asset(
+                  'assets/images/trush.png',
+                  width: 130,
+                  height: 130,
+                  color: Colors.red,
+                )
+              : Icon(
+                  Icons.delete,
+                  color: isNear ? Colors.red : Colors.black,
+                  size: 130,
+                ),
+        ),
+      ],
+    );
+  }
+}
